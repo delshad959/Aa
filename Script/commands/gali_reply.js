6a861712ba84e0c0df3410c1f46d21728893cd26
delshad959 <mdakashhosain915@gmail.com>
2025-11-02 10:20:48 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("gala gali")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dunga)",
+				body: "(খানকির পোলা তোর মায়রে চুদি )"," বেশ্যা মাগির পোলা", 
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
